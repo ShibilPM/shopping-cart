cff0832cf1b9b742a69628ed12f103c656dc70aa
ShibilPM <shibilpm3232@gmail.com>
2020-12-06 07:23:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          Product List
+        </main>
+        <footer>
+          All right is reserved.
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
